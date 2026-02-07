a34c08b400726fcf6615ef018bbed6b4019f6bed
Emilyeme <emilyemersom395@gmail.com>
2026-02-07 15:15:36 -0800

    new

--- server.js
+++ server.js
@@ -6,14 +6,31 @@
  * Require Statements
  *************************/
 const express = require("express")
+const path = require("path")
+const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 
 /* ***********************
+ * View Engines and Templates
+ *************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "layouts/layout")
+app.set('views', path.join(__dirname, 'views'));
+
+/* ***********************
  * Routes
  *************************/
 app.use(static)
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+//Index route
+app.get("/", function (req, res) {
+  res.render("index", { title: "Home" })
+})
 
 /* ***********************
  * Local Server Information
