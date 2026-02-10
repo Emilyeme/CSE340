3d52ea87fb8d106b7c4d15cfc5465d97f36fb1ff
Emilyeme <emilyemersom395@gmail.com>
2026-02-10 15:31:23 -0800

    week2

---
+++ database/assignment2.sql
@@ -0,0 +1,23 @@
+-- Insert Tony Stark record
+INSERT INTO account (account_firstname, account_lastname, account_email, account_password) 
+VALUES ('Tony', 'Stark', 'tony@starkent.com', 'Iam1ronM@n');
+-- Update Tony Stark to Admin 
+UPDATE account 
+SET account_type = 'Admin'
+WHERE account_email = 'tony@starkent.com';
+-- Delete Tony Stark record 
+DELETE FROM account 
+WHERE account_email = 'tony@starkent.com'
+-- Update GM Hummer description 
+UPDATE inventory 
+SET inv_description = REPLACE(inv_description, 'small interiors', 'a huge interior') 
+WHERE inv_make = 'GM' AND inv_model = 'Hummer';
+--  Inner join for Sport category 
+SELECT i.inv_make, i.inv_model, c.classification_name 
+FROM inventory i 
+INNER JOIN classification c ON i.classification_id = c.classification_id 
+WHERE c.classification_name = 'Sport';
+-- Update image and thumbnail paths 
+UPDATE inventory 
+SET inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/'), 
+inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/');
